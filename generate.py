b80fcdd57a9e18ba9ca77d36fa1e55202fe5ad80
jcurbelo <curbelorobin@gmail.com>
2019-03-14 09:29:11 -0400

    Added ability to concat barcodes into html

--- generate.py
+++ generate.py
@@ -1,15 +1,35 @@
 import json
 from barcode import generate
 
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VINs</title>
+</head>
+<body>
+{}
+</body>
+</html>
+"""
+
 
 def main():
     try:
         with open('vins.json', 'r') as f:
+            img_html_tags = []
             vins = json.load(f)
         for vin in vins:
-            generate('CODE39', vin, output='barcodes/{}'.format(vin))
+            name = generate('CODE39', vin, output='barcodes/{}'.format(vin))
+            img_html_tags.append('<img src="{0}" alt={0}/>'.format(name))
     except FileNotFoundError:
         print('"vins.json" file not found.')
+
+    # generates html
+    output = HTML_TEMPLATE.format(''.join(img_html_tags))
+    # writes html file
+    with open('index.html', 'w') as f:
+        f.write(output)
 
 
 main()
