634aad9620294dbf6f82b7356afd22104dc5ce57
jcurbelo <curbelorobin@gmail.com>
2019-04-11 13:59:31 -0400

    Changed barcode for TEC-IT

--- generate.py
+++ generate.py
@@ -1,15 +1,31 @@
 import json
-from barcode import generate
+
+IMG_BASE_URL = 'https://barcode.tec-it.com/barcode.ashx?data={}&code=Code39&dpi=96&dataseparator='
 
 HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>VINs</title>
+	<style>
+		img {{
+			margin: 10px;
+			width: 100%;
+			max-width: 250px;
+		}}
+	</style>
 </head>
+
 <body>
-{}
+	{}
+	<footer>
+		<a href='https://www.tec-it.com' title='Barcode Software by TEC-IT'>
+			Barcode generated with TEC-IT Barcode Software
+		</a>
+	</footer>
 </body>
+
 </html>
 """
 
@@ -19,9 +35,10 @@ def main():
         with open('vins.json', 'r') as f:
             img_html_tags = []
             vins = json.load(f)
-        for vin in vins:
-            name = generate('CODE39', vin, output='barcodes/{}'.format(vin))
-            img_html_tags.append('<img src="{0}" alt={0}/>'.format(name))
+            for vin in vins:
+                img_link = IMG_BASE_URL.format(vin)
+                img_html_tags.append(
+                    '<img src="{}" alt="Barcode Generator TEC-IT"/>'.format(img_link))
     except FileNotFoundError:
         print('"vins.json" file not found.')
 
